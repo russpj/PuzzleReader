9c70d5ffd8a30f282e585ec08995245c555e3a87
Russ Paul-Jones <russ@paul-jones.org>
2022-09-06 16:38:02 -0700

    Changed indentation to spaces

--- PuzzleReader.py
+++ PuzzleReader.py
@@ -8,56 +8,56 @@ from bs4 import BeautifulSoup
 
 
 def GetHtml(filename):
-	with open(filename, 'r', encoding='utf-8') as file:
-		return file.read()
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
 
 
 def FindNumbersInDiv(soup, div):
-	groups = soup.find(id=div)
-	output_groups = []
-	for group in groups.find_all('div', class_='task-group'):
-		output_group = []
-		for cell in group.find_all('div'):
-			if cell.text:
-				output_group.append(cell.text)
-		output_groups.append(output_group)
-	return output_groups	
+    groups = soup.find(id=div)
+    output_groups = []
+    for group in groups.find_all('div', class_='task-group'):
+        output_group = []
+        for cell in group.find_all('div'):
+            if cell.text:
+                output_group.append(cell.text)
+        output_groups.append(output_group)
+    return output_groups	
 
 def ParsePuzzle(text):
-	soup = BeautifulSoup(text, 'html.parser')
+    soup = BeautifulSoup(text, 'html.parser')
 
-	columns = FindNumbersInDiv(soup, 'taskTop')
-	rows = FindNumbersInDiv(soup, 'taskLeft')
+    columns = FindNumbersInDiv(soup, 'taskTop')
+    rows = FindNumbersInDiv(soup, 'taskLeft')
 
-	return (columns, rows)
+    return (columns, rows)
 
 
 def print_groups(groups, file):
-	for group in groups:
-		line = ''
-		for number in group:
-			line += number
-			line += ' '
-		print(line, file=file)
+    for group in groups:
+        line = ''
+        for number in group:
+            line += number
+            line += ' '
+        print(line, file=file)
 
 def write_file(filename, parsed_file):
-	columns = parsed_file[0]
-	rows = parsed_file[1]
+    columns = parsed_file[0]
+    rows = parsed_file[1]
 
-	with open(filename, 'w') as out_file:
-		print(len(rows), file=out_file)
-		print(len(columns), file=out_file)
-		print_groups(rows, file=out_file)
-		print_groups(columns, file=out_file)
+    with open(filename, 'w') as out_file:
+        print(len(rows), file=out_file)
+        print(len(columns), file=out_file)
+        print_groups(rows, file=out_file)
+        print_groups(columns, file=out_file)
 
 
 def Main():
-	puzzle_name = 'Special Monthly Nonograms'
+    puzzle_name = 'small'
 
-	puzzle_html = GetHtml('{}.html'.format(puzzle_name))
-	puzzle_definition = ParsePuzzle(puzzle_html)
-	write_file('{}.txt'.format(puzzle_name), puzzle_definition)
+    puzzle_html = GetHtml('{}.html'.format(puzzle_name))
+    puzzle_definition = ParsePuzzle(puzzle_html)
+    write_file('{}.txt'.format(puzzle_name), puzzle_definition)
 
 
 if __name__ == '__main__':
-	Main()
+    Main()
